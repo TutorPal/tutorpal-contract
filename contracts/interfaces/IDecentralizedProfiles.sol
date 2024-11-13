6cc159d2ee0f9c29af621eec9aeea2f3cdcd313b
JustFavour <odeyemifavour1@gmail.com>
2024-11-13 09:20:53 +0100

    added the auth of admin

--- contracts/interfaces/IDecentralizedProfiles.sol
+++ contracts/interfaces/IDecentralizedProfiles.sol
@@ -16,8 +16,24 @@ interface IDecentralizedProfiles {
     }
 
     error NotANewUser(address user);
+    error NotAdmin();
+    error NotOperator();
 
+    /// @notice Emitted when a new user is registered
+    /// @param user The address of the registered user
+    /// @param displayName The display name of the registered user
+    /// @param roleType The role type of the registered user
     event UserRegistered(address indexed user, string displayName, RoleType roleType);
+
+    /// @notice Emitted when a new admin is added
+    /// @param newAdminAddress The address of the new admin that was added
+    /// @param admin The address of the admin that added the new admin
+    event NewAdmin(address indexed newAdminAddress, address indexed admin);
+
+    /// @notice Emitted when an admin is removed
+    /// @param removedAdmin The address of the admin that was removed
+    /// @param admin The address of the admin that removed the admin
+    event RemovedAdmin(address indexed removedAdmin, address indexed admin);
 
     // Function to register a new user
     function registerUser(string calldata _displayName, RoleType _roleType) external;
@@ -27,4 +43,9 @@ interface IDecentralizedProfiles {
         external
         view
         returns (string memory displayName, RoleType roleType, bool isRegistered);
+
+    function renounceAdminRole() external;
+    function addAdmin(address) external;
+    function removeAdmin(address) external;
+    function isAdmin(address usr) external view returns (bool);
 }
