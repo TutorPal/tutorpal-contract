badb511d438ba6686cdced4b9a4f1888cf22631a
beebozy <musahabeeblai@gmail.com.com>
2024-11-18 17:20:16 +0100

    solidity version conflict

--- contracts/tutorpalSecondaryMarket.sol
+++ contracts/tutorpalSecondaryMarket.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.27;
+pragma solidity 0.8.20;
 
 import {DecentralizedProfiles} from "./abstract/DecentralizedProfiles.sol";
 import {SessionBooking} from "./abstract/SessionBooking.sol";
